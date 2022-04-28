b11a811b8018a8783acb2a8fb647e0c75e74cb14
VictorGenario <victordamatagenario@gmail.com>
2022-04-27 22:27:38 -0300

    Atualizando o Render para o createRoot

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { TaskList } from './components/TaskList'
 import { Header } from "./components/Header";
 import './styles/global.scss'
+import { useEffect, useMemo, useState } from 'react';
+
+
 
 
 export function App() {
   return (
     <>
-      <Header />
+      <Header/>
       <TaskList />
     </>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-import { render } from 'react-dom'
+//import { render } from 'react-dom'
 
 import { App } from './App'
 
-render(<App />, document.getElementById('root'))
+//render(<App />, document.getElementById('root'))
+
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container as HTMLDivElement); // createRoot(container!) if you use TypeScript
+root.render(<App />);
